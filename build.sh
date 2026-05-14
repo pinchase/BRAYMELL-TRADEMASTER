145bc9c53c01685fb043a9aa1b6fd57b6c85e14a
pinchase <kagiripeterson8404@gmail.com>
2026-05-14 14:00:17 +0300

    re-try fixing database issue

--- build.sh
+++ build.sh
@@ -1,14 +1,7 @@
 #!/bin/bash
-# Build script for Render deployment
-
 set -o errexit
 
-# Install Python dependencies
 pip install --upgrade pip
 pip install -r requirements.txt
 
-# # Collect static files
-# python manage.py collectstatic --no-input
-
-# Run migrations
 python manage.py migrate
